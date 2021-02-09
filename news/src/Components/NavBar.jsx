763ec78e97c2cf454cba996186e4abd32602b512
phil-stm <philippestm@yahoo.com>
2021-02-08 16:59:21 -0800

    finished news app.

--- news/src/Components/NavBar.jsx
+++ news/src/Components/NavBar.jsx
@@ -5,14 +5,19 @@ function NavBar() {
     return (
         <nav>
             <h1>TEK News</h1>
-            <span>Add your &lt;Link/&gt;'s to the Pages Here in the NavBar</span>
             <ul>
-                <Link to="/">
-                    <li>Home</li>
-                    </Link>
-                <li><a href="http://bit.ly/teksysacademy" target="_blank" rel="noreferrer">Sports</a></li>
-                <li><a href="http://bit.ly/teksysacademy" target="_blank" rel="noreferrer">Weather</a></li>
-                <li><a href="http://bit.ly/teksysacademy" target="_blank" rel="noreferrer">Politics</a></li>
+                <li>
+                    <Link to="/"><span className="navBar">Home</span></Link>
+                </li>
+                <li>
+                    <Link to="/sports"><span className="navBar">Sports</span></Link>
+                </li>
+                <li>
+                    <Link to="/weather"><span className="navBar">Weather</span></Link>
+                </li>
+                <li>
+                    <Link to="/politics"><span className="navBar">Politics</span></Link>
+                </li>
             </ul>
         </nav>
     )
